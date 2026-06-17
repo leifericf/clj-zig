41fd9058b7cccdb3a1b205be0a901be131e1d573
Leif Eric Fredheim <463422+leifericf@users.noreply.github.com>
2026-06-17 10:53:38 +0200

    Build: Set up Clojars publishing for clj-zig

---
+++ build.clj
@@ -0,0 +1,53 @@
+(ns build
+  "Release build for clj-zig itself. `jar` packages the library; `install`
+  is the local dry-run that puts the jar in the local Maven repository
+  without publishing; `deploy` publishes to Clojars and needs
+  CLOJARS_USERNAME and CLOJARS_PASSWORD, run by the maintainer. The git-dep
+  coordinate keeps working for contributors regardless."
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
+
+(def lib 'io.github.leifericf/clj-zig)
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (delay (b/create-basis {:project "deps.edn"})))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean
+  "Remove build outputs."
+  [_]
+  (b/delete {:path "target"}))
+
+(defn jar
+  "Write the pom and package the sources into a jar."
+  [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]
+                :scm {:url "https://github.com/leifericf/clj-zig"
+                      :connection "scm:git:git://github.com/leifericf/clj-zig.git"
+                      :developerConnection "scm:git:ssh://git@github.com/leifericf/clj-zig.git"
+                      :tag (str "v" version)}})
+  (b/copy-dir {:src-dirs ["src"] :target-dir class-dir})
+  (b/jar {:class-dir class-dir :jar-file jar-file}))
+
+(defn install
+  "Build the jar and install it into the local Maven repository. The local
+  dry-run that proves the artifact builds and resolves without publishing."
+  [_]
+  (clean nil)
+  (jar nil)
+  (b/install {:basis @basis :lib lib :version version
+              :class-dir class-dir :jar-file jar-file}))
+
+(defn deploy
+  "Build the jar and publish it to Clojars. Needs CLOJARS_USERNAME and
+  CLOJARS_PASSWORD in the environment; run by the maintainer."
+  [_]
+  (clean nil)
+  (jar nil)
+  (dd/deploy {:installer :remote
+              :artifact jar-file
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
